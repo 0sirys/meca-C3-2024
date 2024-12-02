1bbad3972adf0b2ae320f2740865f6835070482d
0sirys <jlpimentel02@hotmail.com>
2024-12-02 15:03:04 -0400

    Hot fix Toggle_Buttons

--- Arduino/Arduino/ESP32_Carrito/ESP32_Carrito.ino
+++ Arduino/Arduino/ESP32_Carrito/ESP32_Carrito.ino
@@ -1,43 +1,49 @@
-#include <RF24.h>
-#define CE_PIN 4
-#define CSN_PIN 5
-#define IRQ_PIN 10
-#define MOSI_PIN 23
-#define MISO_PIN 19
-#define SCK_PIN 18
+#include <PS4Controller.h>
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+
 #define INTERMITENTES 10
 //retencion de pulso en cualquier
 class Button {
 private:
+  uint8_t state_input = 0;
   int pinbutton;
+  uint8_t previousState = 0;  // ultimo estado del boton
+  uint8_t currentState = 1;   // estado actual del boton
+  unsigned long lapse = 0;
 public:
-  uint8_t previousState = HIGH;  // ultimo estado del boton
-  uint8_t currentState = LOW;    // estado actual del boton
-  unsigned long lapse = 0;       // track control del rebote de boton
-  Button(int pin) {              // constructor
+  // track control del rebote de boton
+  Button(int pin) {  // constructor
     this->pinbutton = pin;
+    pinMode(pin, OUTPUT);
   };
   // Funcion para actualizar el estado del boton
-  uint8_t setState(uint8_t mode) {
-    if (millis() - lapse >= 24) {  // Intervalo de rebote
-      lapse = millis();            // reinicio de conteo
+  uint8_t Toggle(uint8_t mode) {
+    this->currentState = mode;
 
-      if (mode > 0) {                              // condicion para solo activar en high
-        uint8_t pivot = this->previousState;       // pivote para acarreo de estado.
-        this->previousState = this->currentState;  //acarreo de estado actual a anterior.
-        this->currentState = pivot;                // modifico el estado actual 1 o 0
-        return this->currentState;                 // devuelvo el nuevo estado anclado.
+    if (this->currentState == 0 && this->previousState == 1) {
+      this->state_input = 1 - this->state_input;
+
+      if (this->state_input == 1) {
+        digitalWrite(this->pinbutton, HIGH);
+      } else {
+        digitalWrite(this->pinbutton, LOW);
       }
+      this->previousState = this->currentState;
+      return this->currentState;
     }
+
     return this->currentState;  // devuelvo el estado anclado.
   };
+
   int pin() {
     return this->pinbutton;
   };
 };
 
-//propiedades y funciones del motor
-class Motor {
+
+class Motor {  //propiedades y funciones del motor
 private:
   uint8_t d01;
   uint8_t d02;
@@ -56,77 +62,52 @@ public:
       analogWrite(this->d01, D1);
       return D1;
     }
+    if (D2 == D1) {
+      analogWrite(this->d01, 0);
+      analogWrite(this->d02, 0);
+      return -1;
+    }
     return 0;
   }
 };
 
-typedef struct data_controller {
-  byte R2_gas;
-  byte L2_brake;
-  byte axys;
-  byte botones;
-} Controllerr;
-//objeto controllerr
-Controllerr buffer;
 
-//objeto nrf24L01
-RF24 radio(CE_PIN, CSN_PIN);
-//direccion de antena
-const uint64_t address = 0xE8E8F0F0E1LL;
+Motor rightEdge;  //Motor 1
+Motor leftEdge;   // Motor 2
 
-//Motores 1 y 2 independientes
-Motor rightEdge;
-Motor leftEdge;
-// botones con retencion
-Button buttonkeeper(INTERMITENTES);
 
-//inicializacion de parametros
-void setparameters() {
-  buffer.axys = 127;    // centro del joystick
-  buffer.L2_brake = 0;  // reversa y freno
-  buffer.R2_gas = 0;    // avance
-  buffer.botones = 0;   // intermitentes
-};
-// estatus de antena de radio frecuencia
-void radioStatus() {
-  if (!radio.begin()) { // condicion esta
-    Serial.println("Conexion Nrf24 fallida");
-    while (1) {};
+Button buttonkeeper(INTERMITENTES);  // botone con retencion
+
+void removePairedController() {  // desvincula los mandos conectados.
+  uint8_t pairedDeviceBtAddr[20][6];
+  int count = esp_bt_gap_get_bond_device_num();
+  esp_bt_gap_get_bond_device_list(&count, pairedDeviceBtAddr);
+  for (int i = 0; i < count; i++) {
+    esp_bt_gap_remove_bond_device(pairedDeviceBtAddr[i]);
   }
-  Serial.print("!!Conexion Nrf24 exitosa!!\n");
-  radio.setChannel(100);
-  radio.setAutoAck(false);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.openReadingPipe(1, address);
-  radio.startListening();
 }
-// funciones del carrito
-void Duty() {
-  rightEdge.marcha(buffer.R2_gas, buffer.L2_brake);
-  leftEdge.marcha(buffer.R2_gas, buffer.L2_brake);
-  digitalWrite(buttonkeeper.pin(), buttonkeeper.setState(buffer.botones));
+
+void Duty() {  // funciones del carrito
+  char release[100];
+  sprintf(release, "R2: %d, L2:%d, cross:%d,\n", PS4.R2Value(), PS4.L2Value(), buttonkeeper.Toggle(PS4.Cross()));
+  Serial.print(release);
+  rightEdge.marcha(PS4.R2Value(), PS4.L2Value());
+  leftEdge.marcha(PS4.R2Value(), PS4.L2Value());
 }
 
 
-void setup() {
+
+void setup() {  // Inicializacion de esp32
   rightEdge.setOutput(15, 13);
   leftEdge.setOutput(0, 2);
-  setparameters();
   Serial.begin(115200);
-  radioStatus();
-}
-//obtiene los datos y los almacena en el buffer
-void receptor() {
-  while (radio.available()) {
-    radio.read(&buffer, sizeof(buffer));
-    char release[100];
-    sprintf(release, "R2: %d, L2:%d, Botones:%d, axisX:%d\n", buffer.R2_gas, buffer.L2_brake, buttonkeeper.setState(buffer.botones), buffer.axys);
-    Serial.print(release);
-    Duty();
+  PS4.begin();
+  removePairedController();
+  while (!PS4.isConnected()) {
+    Serial.print("Wait Controller");
   }
 }
 
 void loop() {
-  receptor();
+  Duty();
 }
